10af2fa261b6d180dbbf65a90bf6777bafb79929
Ilmi-Restu-Firdaus <151982742+Ilmi-Restu-Firdaus@users.noreply.github.com>
2024-02-08 01:08:33 +0700

    Update KODIGAN_UAS_SD.java

--- KODIGAN_UAS_SD.java
+++ KODIGAN_UAS_SD.java
@@ -11,15 +11,17 @@ public class SD {
         Scanner scanner = new Scanner(System.in);
          
         record tambah( String Jenis,String Nama,int Jumlah,int Harga,int total){}
-        
+        boolean toko = true;
         ArrayList<String> riwayatBarang = new ArrayList<>();
         LinkedList<tambah> tambahbarang = new LinkedList<>();
         int jumlahKeluar,barang;
         
         
-          while (true) {
-            
-            System.out.println("\n1. Beli/tambah stok barang");
+          while (toko) {
+            System.out.println("\033[32m┌───────────────────────────────────────┐\033[0m");  
+            System.out.println("            TOKO GLOSIR ");
+            System.out.println("\033[32m└───────────────────────────────────────┘\033[0m");                                   
+            System.out.println("1. Beli/tambah stok barang");
             System.out.println("2. Tampilkan stok barang");
             System.out.println("3. Jual/Keluarkan barang");
             System.out.println("4. Riwayat barang");
@@ -53,7 +55,7 @@ public class SD {
             int total = jumlahbarang * hargabarang;
             
             System.out.println("Total Belanja                     : " + total);
-             String riwayatMasuk = "[+] " +"jenis :" + jenisbarang + ",Nama :" + namabarang + ", Jumlah :" + jumlahbarang+", Harga :"+ hargabarang+", total belanja: "+ total+ " ditambahkan ke gudang.";
+             String riwayatMasuk = "[+] " +"jenis :" + jenisbarang + ",Nama :" + namabarang + ", Jumlah :" + jumlahbarang+", Harga :"+ hargabarang+", total Pembelian: "+ total+ " ditambahkan ke gudang.";
             riwayatBarang.add(riwayatMasuk);
              tambahbarang.add(new tambah(jenisbarang,namabarang,jumlahbarang,hargabarang,total));              
             System.out.println(riwayatMasuk + " Barang berhasil di tambahkan");       
@@ -69,15 +71,13 @@ public class SD {
             case 2:
                 
            if(!tambahbarang.isEmpty()){
-              System.out.println("Barang masuk");
+              System.out.println("Stok Barang");
                      for (tambah item : tambahbarang) {
             if (item.Jumlah() > 0) {
-                System.out.println("Jenis: " + item.Jenis() + ", Nama: " + item.Nama() +
-                        ", Jumlah: " + item.Jumlah() + ", harga: " + item.Harga() +
-                        ", Total Harga: " + item.total());
+                System.out.println("Jenis: " + item.Jenis() + ", Nama: " + item.Nama() + ", Jumlah: " + item.Jumlah() + ", harga: " + item.Harga() + ", Total Penjualan: " + item.total());
+                }
             }
-        }
-            }else{
+                }else {
                     System.out.println("belum ada barang masuk");
             }
                     System.out.println("1.Cari barang ");
@@ -93,35 +93,31 @@ public class SD {
                     String nam = scanner.next();    
                     
                     boolean found = false;
-        for (tambah item : tambahbarang) {
-            if (item.Nama().equalsIgnoreCase(nam)&& item.Jenis().equalsIgnoreCase(jen)) {
-                found = true;
-            System.out.println("Jenis: " + item.Jenis()  + ", Nama: " + item.Nama() + ", Jumlah: " + item.Jumlah() + ", harga: " + item.Harga()  +", Total Harga: "+ item.total());
-            }
-        }
-        if (!found) {
-            System.out.println("Barang tidak di temukan");
-        }break;
-    }
-            else if (barang == 2) {
-                
-            tambahbarang.sort((item1, item2) -> item1.Jenis().compareTo(item2.Jenis()));
-            System.out.println("Stok Barang setelah diurutkan berdasar makanan/minuman:");
-        for (tambah item : tambahbarang) {
-            System.out.println("Jenis: " + item.Jenis() + ", Nama: " + item.Nama() +
-                                    ", Jumlah: " + item.Jumlah() + ", harga: " + item.Harga() +
-                                    ", Total Harga: " + item.total());
-                        }
+             for (tambah item : tambahbarang) {
+                 if (item.Nama().equalsIgnoreCase(nam)&& item.Jenis().equalsIgnoreCase(jen)) {
+                     found = true;
+                 System.out.println("Jenis: " + item.Jenis()  + ", Nama: " + item.Nama() + ", Jumlah: " + item.Jumlah() + ", harga: " + item.Harga()  +", Total Harga: "+ item.total());
                     }
-           else if (barang ==3 ) {
+                 }
+                  if (!found) {
+                 System.out.println("Barang tidak di temukan");
+                 }break;
+                 }
+                    
+                else if (barang == 2) {  
+                tambahbarang.sort((item1, item2) -> item1.Jenis().compareTo(item2.Jenis()));
+                System.out.println("Stok Barang setelah diurutkan berdasar makanan/minuman:");
+            for (tambah item : tambahbarang) {
+                System.out.println("Jenis: " + item.Jenis() + ", Nama: " + item.Nama() + ", Jumlah: " + item.Jumlah() + ", harga: " + item.Harga() + ", Total Harga: " + item.total());
+                            }
+                        }
+                else if (barang ==3 ) {
              }break;       
                     
             case 3:
                 
             System.out.print("Masukkan jenis barang yang akan dikeluarkan (Makanan/Minuman): ");
             String jenisKeluar = scanner.next();
-            
-            
             if (jenisKeluar.equalsIgnoreCase("makanan") || jenisKeluar.equalsIgnoreCase("minuman")) {
             System.out.print("Masukkan nama barang yang akan dikeluarkan                   : ");
             String namaKeluar = scanner.next();
@@ -129,7 +125,6 @@ public class SD {
             jumlahKeluar = scanner.nextInt();
             System.out.print("masukan harga jual satuan                                    : ");
             int hargajual = scanner.nextInt();
-            
             boolean barangDitemukan = false;
 
         for (tambah item : tambahbarang) {
@@ -179,6 +174,9 @@ public class SD {
                         }
                     }
                     break;    
-          }    
-      }
+          }    System.out.print("Kembali ke Menu Utama? (y/n): ");
+            String jawaban = scanner.next().toLowerCase();
+            toko = jawaban.equals("y");
+      } 
+            scanner.close();
 }}
